f7605a31414ead6ac271f6fa49953a164f03b083
supernovabee <dosin.1ne@gmail.com>
2025-07-21 22:35:08 -0500

    Added comments that explain my thought process as to what I want this code to accomplish

--- dream-app.js
+++ dream-app.js
@@ -2,7 +2,7 @@
 const readline = require('readline-sync'); //I want the user to be able to pick a type of candy to hand to the NPC (Values,Data Types, and Operations)
 
 let inventory = [
-    ["keys", "chocolate", "gummy bears"],                       //a nested Array is used as an inventory (Building Arrays)
+    ["keys", "chocolate", "gummy bears"],                       //a nested Array is used as an inventory (Building Arrays, Using Arrays)
 
     ["breath mints", "loose change", "wallet"]
 ];
@@ -17,3 +17,23 @@ if (item = chocolate) {                                                    //thi
     else {
   console.log("Oh...I guess that's okay too");
 }
+
+
+for (let day = 0; day > 12; day++) {                //a loop to calculate when night switches to day. (Working with loops)
+  console.log("It's nighttime")
+};
+
+
+
+
+if (String.length >= 10) {                                                    //this if/else statement compares a user-inputed string's length to 10 to determine if the NPC likes the name or not
+  console.log("That sounds like a great name for our project!");                     // (Stringing Characters together)
+} 
+    else {
+  console.log("Mmmm, I don't know, I think its missing something");
+};
+
+
+//This isn't yet working code, a bit of pseudocode so I can get my thought process and ideas on paper.
+//I know these most likely won't work and have a bunch of syntax errors but they provide a framework for me to reference
+//when making my game!
